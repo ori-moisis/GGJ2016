9799461dcd26da204b08a257e23781cc07f2e1c2
ori-moisis <ori_moisis@hotmail.com>
2016-01-29 21:22:36 +0200

    asD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ using System;
 public class PlayerController : MonoBehaviour {
 
 	public ArrayList danceMoves;
-	public GameObject beatBar;
 	public GameObject wooweeObject;
 	WooeeController woowee;
 
@@ -18,41 +17,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp(KeyCode.A)) {
-			HandleButton ("jump");
-		}
-		if (Input.GetKeyUp(KeyCode.S)) {
-			HandleButton ("kick");
-		}
     }
-
-	float getMoveAccuracy() {
-		// TODO: use the beat bar script
-		return 0.75f;
-	}
-
-	public void HandleButton(string moveType) {
-		Debug.Log ("Got button " + moveType);
-
-		DanceMove danceMove = DanceMove.Jump;
-		switch (moveType) {
-		case "jump":
-			danceMove = DanceMove.Jump;
-			break;
-		case "kick":
-			danceMove = DanceMove.Kick;
-			break;
-		}
-
-		float moveAcc = getMoveAccuracy ();
-		doDanceMove (danceMove, moveAcc);
-	}
-
+		
 	void animateDanceMove(DanceMove danceMove) {
 		
 	}
 
-	void doDanceMove(DanceMove danceMove, float accuracy) {
+	public void doDanceMove(DanceMove danceMove, float accuracy) {
 		animateDanceMove (danceMove);
 		danceMoves.Add (danceMove);
 		woowee.reactToMove (danceMove, accuracy, this);
