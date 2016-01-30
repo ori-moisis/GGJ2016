3659b12d2254e06bb8180a93939ed1b69f9c2485
ori-moisis <ori_moisis@hotmail.com>
2016-01-30 17:53:17 +0200

    sounds

--- Assets/Scripts/MessageController.cs
+++ Assets/Scripts/MessageController.cs
@@ -6,12 +6,20 @@ public enum TextOverlayType { Hit, Miss, Fail, Combo, Lose, Win };
 
 public class MessageController : MonoBehaviour {
     public Sprite[] hitSprites;
+	public AudioClip[] hitSounds;
     public Sprite[] missSprites;
+	public AudioClip[] missSounds;
     public Sprite[] failSprites;
+	public AudioClip[] failSounds;
     public Sprite[] comboSprites;
+	public AudioClip[] comboSounds;
 	public Sprite[] loseSprites;
+	public AudioClip[] loseSounds;
 	public Sprite[] winSprites;
+	public AudioClip[] winSounds;
 	public Image image;
+	public AudioSource audioSrc;
+
 
 	private Animator animator;
 
@@ -24,34 +32,49 @@ public class MessageController : MonoBehaviour {
     public void showRandomOverlayTextOfType(TextOverlayType type)
     {
         Sprite[] sprites = null;
+		AudioClip[] sounds = null;
 
         switch (type)
         {
-            case TextOverlayType.Hit:
-                sprites = hitSprites;
-                break;
-            case TextOverlayType.Miss:
-                sprites = missSprites;
-                break;
-            case TextOverlayType.Fail:
-                sprites = failSprites;
-                break;
-            case TextOverlayType.Combo:
-                sprites = comboSprites;
-                break;
-			case TextOverlayType.Win:
-				sprites = winSprites;
-				break;
-			case TextOverlayType.Lose:
-				image.sprite = null;
-				image.overrideSprite = null;
-				animator.SetTrigger("nopeMessage");
-				return;
+		case TextOverlayType.Hit:
+			sprites = hitSprites;
+			sounds = hitSounds;
+            break;
+		case TextOverlayType.Miss:
+			sprites = missSprites;
+			sounds = missSounds;
+            break;
+		case TextOverlayType.Fail:
+			sprites = failSprites;
+			sounds = failSounds;
+            break;
+		case TextOverlayType.Combo:
+			sprites = comboSprites;
+			sounds = comboSounds;
+            break;
+		case TextOverlayType.Win:
+			sprites = winSprites;
+			sounds = winSounds;
+			break;
+		case TextOverlayType.Lose:
+			image.sprite = null;
+			image.overrideSprite = null;
+			animator.SetTrigger ("nopeMessage");
+			sounds = loseSounds;
+			break;
         }
+
+		if (sounds == null || sounds.Length == 0) {
+			// Should not happend
+			return;
+		}
+
+		int index = Random.Range (0, sounds.Length - 1);
+		audioSrc.PlayOneShot (sounds [index]);
 
         if (sprites != null && sprites.Length > 0)
         {
-            Sprite sprite = sprites[Random.Range(0, sprites.Length - 1)];
+            Sprite sprite = sprites[index];
             if (sprite)
             {
 				image.sprite = sprite;
