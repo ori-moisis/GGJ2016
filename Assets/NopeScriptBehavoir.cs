cd219d624dd9ff089d4892b0f84ef9016f6faead
ori-moisis <ori_moisis@hotmail.com>
2016-01-30 18:59:02 +0200

    asd

--- Assets/NopeScriptBehavoir.cs
+++ Assets/NopeScriptBehavoir.cs
@@ -5,7 +5,7 @@ public class NopeScriptBehavoir : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		UnityEngine.SceneManagement.SceneManager.LoadScene ("splash");
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("Start");
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
