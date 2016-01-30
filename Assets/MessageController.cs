101fd3cde73837288ea0a8209712ed9db79574ea
Barak Yoresh <barakyoresh@gmail.com>
2016-01-30 02:48:38 +0200

    Add overlay text manager.

---
+++ Assets/MessageController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum TextOverlayType { Hit, Miss, Fail, Combo }
+
+public class MessageController : MonoBehaviour {
+    public Sprite[] hitSprites;
+    public Sprite[] missSprites;
+    public Sprite[] failSprites;
+    public Sprite[] comboSprites;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public void showRandomOverlayTextOfType(TextOverlayType type)
+    {
+        Sprite[] sprites = null;
+
+        switch (type)
+        {
+            case TextOverlayType.Hit:
+                sprites = hitSprites;
+                break;
+            case TextOverlayType.Miss:
+                sprites = missSprites;
+                break;
+            case TextOverlayType.Fail:
+                sprites = failSprites;
+                break;
+            case TextOverlayType.Combo:
+                sprites = comboSprites;
+                break;
+        }
+
+        if (sprites != null && sprites.Length > 0)
+        {
+            Sprite sprite = sprites[Random.Range(0, sprites.Length)];
+            if (sprite)
+            {
+                Image image = this.GetComponent<Image>();
+                image.sprite = sprite;
+                this.GetComponent<Animator>().SetTrigger("showMessage");
+            }
+        }
+    }
+}
