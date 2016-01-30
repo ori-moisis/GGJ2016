4f9501ff7193576bc64b3e781f8dc1498b23940d
Itzik Ephraim <oranja@gmail.com>
2016-01-30 18:42:08 +0200

    Failing splash screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame() {
+		Debug.Log ("Start game");
+		SceneManager.LoadScene ("Project");
+
+//		SceneManager.UnloadScene ("Start");
+	}
+}
